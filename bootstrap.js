e9a36b3410267aa3271e204df2300a8310e05f86
NemoVonNirgend <207980572+NemoVonNirgend@users.noreply.github.com>
2026-07-19 20:13:46 -0230

    wire lorebook repository

--- bootstrap.js
+++ bootstrap.js
@@ -1,9 +1,22 @@
+import { chat_metadata, saveMetadata } from '../../../../script.js';
+import {
+    createNewWorldInfo,
+    deleteWorldInfo,
+    loadWorldInfo,
+    saveWorldInfo,
+    createWorldInfoEntry,
+    updateWorldInfoList,
+    METADATA_KEY,
+} from '../../../world-info.js';
+
 import { MODULE_NAME } from './src/core/constants.js';
 import { createLogger } from './src/core/logger.js';
 import { createSettings } from './src/core/settings.js';
 import { createNemoLoreState } from './src/core/state.js';
 import { createLifecycle } from './src/core/lifecycle.js';
+import { createWorldInfoAdapter } from './src/integrations/world-info-adapter.js';
 import { createNounDetector } from './src/lore/noun-detector.js';
+import { createLorebookRepository } from './src/lore/lorebook-repository.js';
 import { createHighlighter } from './src/ui/highlighting.js';
 import { createNotificationCenter } from './src/ui/notification-center.js';
 import { createPopupCoordinator } from './src/ui/popup-coordinator.js';
@@ -12,6 +25,26 @@ const logger = createLogger({ moduleName: MODULE_NAME });
 const settings = createSettings();
 const state = createNemoLoreState({ logger });
 const lifecycle = createLifecycle({ logger, state });
+
+const worldInfo = createWorldInfoAdapter({
+    createWorld: createNewWorldInfo,
+    deleteWorld: deleteWorldInfo,
+    loadWorld: loadWorldInfo,
+    saveWorld: saveWorldInfo,
+    createEntry: createWorldInfoEntry,
+    updateWorldList: updateWorldInfoList,
+    logger,
+});
+
+const lorebooks = createLorebookRepository({
+    adapter: worldInfo,
+    metadata: chat_metadata,
+    saveMetadata,
+    metadataKey: METADATA_KEY,
+    state,
+    logger,
+});
+
 const nounDetector = createNounDetector({ settings, logger });
 const highlighter = createHighlighter({ settings, state, logger });
 const notifications = createNotificationCenter({ logger });
@@ -30,6 +63,8 @@ globalThis.NemoLore = Object.freeze({
     state,
     lifecycle,
     services: Object.freeze({
+        worldInfo,
+        lorebooks,
         nounDetector,
         highlighter,
         notifications,
