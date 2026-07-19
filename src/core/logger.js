5c0f1417590a9bd98f76f3fc50aeb822cceea831
NemoVonNirgend <207980572+NemoVonNirgend@users.noreply.github.com>
2026-07-19 19:52:33 -0230

    normalize logger service interface

--- src/core/logger.js
+++ src/core/logger.js
@@ -10,10 +10,37 @@ export function debug(...args) {
     if (debugEnabled) console.debug(`[${MODULE_NAME}]`, ...args);
 }
 
+export function info(...args) {
+    console.info(`[${MODULE_NAME}]`, ...args);
+}
+
 export function warn(...args) {
     if (debugEnabled) console.warn(`[${MODULE_NAME}]`, ...args);
 }
 
 export function error(...args) {
     console.error(`[${MODULE_NAME}]`, ...args);
+}
+
+export function createLogger({ moduleName = MODULE_NAME, debug: initialDebug = false } = {}) {
+    let localDebugEnabled = Boolean(initialDebug);
+    const prefix = `[${moduleName}]`;
+
+    return Object.freeze({
+        setDebugEnabled(enabled) {
+            localDebugEnabled = Boolean(enabled);
+        },
+        debug(...args) {
+            if (localDebugEnabled) console.debug(prefix, ...args);
+        },
+        info(...args) {
+            console.info(prefix, ...args);
+        },
+        warn(...args) {
+            if (localDebugEnabled) console.warn(prefix, ...args);
+        },
+        error(...args) {
+            console.error(prefix, ...args);
+        },
+    });
 }
