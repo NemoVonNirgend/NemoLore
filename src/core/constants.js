947239cce54f178507eb513ada472c987c9296d1
NemoVonNirgend <207980572+NemoVonNirgend@users.noreply.github.com>
2026-07-19 19:43:32 -0230

    add core constants module

---
+++ src/core/constants.js
@@ -0,0 +1,13 @@
+export const MODULE_NAME = 'NemoLore';
+export const EXTENSION_NAME = 'nemolore';
+export const NEMOLORE_MACRO = 'NemoLore';
+export const NEMOLORE_LOREBOOK_PREFIX = '_NemoLore_';
+
+export const MEMORY_LEVELS = Object.freeze({
+    INDIVIDUAL: 1,
+    PAIRED: 2,
+    CHUNK: 3,
+    SECTION: 4,
+});
+
+export const ERROR_RECOVERY_INTERVAL_MS = 300_000;
